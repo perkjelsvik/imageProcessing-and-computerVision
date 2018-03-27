320ec05ab7cf8a6c7403fbd2834441c40acf5669
perkjelsvik <per.arne.kjelsvik@gmail.com>
2018-03-27 11:49:47 +0200

    restructured inital files

---
+++ code/main_code.m
@@ -0,0 +1,11 @@
+% 3D Face Reconstruction
+% Course: Image Processing & Computer Vision
+
+% Authors: Per s........ & Tiago Machado s2041596
+
+%%
+clear variables
+close all
+clc
+
+stereoCameraCalibrator
