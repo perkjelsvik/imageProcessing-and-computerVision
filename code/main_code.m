e64c575c07fd4ab234212899aeef20bb71c10977
WhalesVagina <t.venturadasilvaribeiromachado@student.utwente.nl>
2018-03-27 12:28:35 +0200

    added some code to main code

--- code/main_code.m
+++ code/main_code.m
@@ -1,11 +1,28 @@
 % 3D Face Reconstruction
 % Course: Image Processing & Computer Vision
 
-% Authors: Per s........ & Tiago Machado s2041596
+% Authors: Per Arne s....... & Tiago Machado s2041596
 
 %%
 clear variables
 close all
 clc
 
-stereoCameraCalibrator
+stereoCameraCalibrator('calibrationSession1_LM')
+stereoCameraCalibrator('calibrationSession1_LR')
+stereoCameraCalibrator('calibrationSession1_MR')
+stereoCameraCalibrator('calibrationSession2_LM')
+stereoCameraCalibrator('calibrationSession2_LR')
+stereoCameraCalibrator('calibrationSession2_MR')
+
+images = imageSet('test');
+imLeft = read(images,1);
+imMiddle = read(images,2);
+imRight = read(images,3);
+
+[J1,J2] = rectifyStereoImages(imLeft,imMiddle,stereoParams1_LM,'OutputView','full');
+disparityMap = disparity(rgb2gray(J1),rgb2gray(J2),'BlockSize',15,'DisparityRange',[-6 10]);
+imshow(disparityMap,[-6 10])
+figure,
+imshow(stereoAnaglyph(J1,J2))
+
